3983cffc77be8ea9d01368c8ece9577c2d685065
Dimitrii Kozlov <ikzdimitrij@gmail.com>
2018-02-15 19:37:57 +0200

    Add Model for Json response

--- WeatherForecast/Controllers/HomeController.cs
+++ WeatherForecast/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using WeatherForecast.Models;
 
 namespace WeatherForecast.Controllers
 {
@@ -14,9 +19,24 @@ namespace WeatherForecast.Controllers
         }
 
         [HttpGet]
-        public ActionResult WeatherForecastByCityName(string cityName)
+        public async Task<ActionResult> WeatherForecastByCityName(string cityName)
         {
-            return HttpNotFound();
+            using (var client = new HttpClient())
+            {
+                var apiKey = "e28d685fe4ed068c63abd4d24b35d7ba";
+                var uri = new Uri($"http://api.openweathermap.org/data/2.5/forecast?q={cityName}&appid={apiKey}");
+
+                var response = await client.GetAsync(uri);
+
+                if (!response.IsSuccessStatusCode)
+                    return HttpNotFound();
+
+                var responseResult = await response.Content.ReadAsStringAsync();
+
+                var wfc = JsonConvert.DeserializeObject<WeatherForecastModel>(responseResult);
+
+                return View("Index", wfc);
+            }
         }
     }
 }
---
+++ WeatherForecast/Models/WeatherForecastModel.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+
+namespace WeatherForecast.Models
+{
+    public class WeatherForecastModel
+    {
+        public string Cod { get; set; }
+        public double Message { get; set; }
+        public long Cnt { get; set; }
+        public List<List> List { get; set; }
+        public City City { get; set; }
+    }
+
+    public class City
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public Coord Coord { get; set; }
+        public string Country { get; set; }
+    }
+
+    public class Coord
+    {
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+    }
+
+    public class List
+    {
+        public long Dt { get; set; }
+        public Main Main { get; set; }
+        public List<Weather> Weather { get; set; }
+        public Clouds Clouds { get; set; }
+        public Wind Wind { get; set; }
+        public Sys Sys { get; set; }
+        public System.DateTime DtTxt { get; set; }
+        public Rain Rain { get; set; }
+        public Rain Snow { get; set; }
+    }
+
+    public class Clouds
+    {
+        public long All { get; set; }
+    }
+
+    public class Main
+    {
+        public double Temp { get; set; }
+        public double TempMin { get; set; }
+        public double TempMax { get; set; }
+        public double Pressure { get; set; }
+        public double SeaLevel { get; set; }
+        public double GrndLevel { get; set; }
+        public long Humidity { get; set; }
+        public double TempKf { get; set; }
+    }
+
+    public class Rain
+    {
+        public double? The3H { get; set; }
+    }
+
+    public class Sys
+    {
+        public string Pod { get; set; }
+    }
+
+    public class Weather
+    {
+        public long Id { get; set; }
+        public string Main { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+
+    public class Wind
+    {
+        public double Speed { get; set; }
+        public double Deg { get; set; }
+    }
+}
